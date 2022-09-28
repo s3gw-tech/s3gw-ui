502470d0de62e6cccf8114e14e7c4cddfb8d0c1e
Volker Theile <vtheile@suse.com>
2022-09-28 13:39:46 +0200

    UI: visual updates to tables
    Fixes: https://github.com/aquarist-labs/s3gw/issues/124
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- src/app/shared/components/navigation-bar/navigation-bar.component.ts
+++ src/app/shared/components/navigation-bar/navigation-bar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { marker as TEXT } from '@ngneat/transloco-keys-manager/marker';
 
 import { NavItem } from '~/app/shared/components/navigation-bar/navigation-bar-item/navigation-bar-item.component';
+import { Icon } from '~/app/shared/enum/icon.enum';
 
 @Component({
   selector: 's3gw-navigation-bar',
@@ -12,17 +13,17 @@ export class NavigationBarComponent {
   navItems: NavItem[] = [
     {
       name: TEXT('Dashboard'),
-      icon: 'mdi mdi-apps',
+      icon: Icon.apps,
       route: '/dashboard'
     },
     {
       name: TEXT('Users'),
-      icon: 'mdi mdi-account-multiple',
+      icon: Icon.users,
       route: '/users'
     },
     {
       name: TEXT('Buckets'),
-      icon: 'mdi mdi-bucket',
+      icon: Icon.bucket,
       route: '/buckets'
     }
   ];
